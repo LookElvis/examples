edac9e0af07e732cdb5d8f8626d349b816413b2c
lucian <504866487@qq.com>
2019-05-20 21:16:26 +0800

    modify

--- examples-spark/src/main/scala/CommonTest/test1.scala
+++ examples-spark/src/main/scala/CommonTest/test1.scala
@@ -13,8 +13,11 @@ object test1 {
 
     //Create the first rdd from the text
     val rdd = sc.parallelize(List("dog","an","cat","an","cat"))
-
     rdd.sortBy(_.charAt(0), true).foreach(println)
+
+    val rdd1 = sc.parallelize(List(("dog", "1"), ("cat", "2"), ("pig", "3")))
+    val rdd2 = sc.parallelize(List(("apple", "4"), ("cat", "5"), ("pig", "6")))
+    val result = rdd1.join(rdd2).foreach(println)
 
     sc.stop()
 
