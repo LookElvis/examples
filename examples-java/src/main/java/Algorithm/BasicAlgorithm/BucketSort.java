72c77e3647b91d391b1a7bb0f896ba4a6cec1a8c
lucian <504866487@qq.com>
2019-05-27 21:40:39 +0800

    modify bucket sort

--- examples-java/src/main/java/Algorithm/BasicAlgorithm/BucketSort.java
+++ examples-java/src/main/java/Algorithm/BasicAlgorithm/BucketSort.java
@@ -7,8 +7,11 @@ package Algorithm.BasicAlgorithm;
 public class BucketSort {
     public static void main(String[] args) {
         int a[] = {1, 2, 2, 7, 4, 9, 3, 5};
+        //设定数组最大值，若不直接指定，可以用一个O(n)的函数来求最大值
         int max = 9;
+        //调用桶排序算法
         bucketSort(a, max);
+        //输出排序后结果
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " ");
         }
@@ -17,11 +20,15 @@ public class BucketSort {
     public static void bucketSort(int a[], int max) {
         int b[] = new int[max + 1];
         int count = 0;
+
+        //将数据分配到桶中
         for (int i = 0; i < a.length; i++) {
             int j = a[i];
+            //对每个桶进行计数，桶中存储的该桶应有的数据个数
             b[j] += 1;
         }
 
+        //对桶进行遍历，根据桶的顺序可以顺序输出数据
         for (int i = 0; i <= max; i++) {
             if (b[i] > 0) {
                 for (int j = 0; j < b[i]; j++) {
@@ -32,3 +39,4 @@ public class BucketSort {
         }
     }
 }
+
