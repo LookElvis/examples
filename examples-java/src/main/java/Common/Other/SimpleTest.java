b67ec01929cfe1637910d8b1a52ba85c24589e9a
lucian <504866487@qq.com>
2019-12-12 11:16:21 +0800

    update leetcode M279

--- examples-java/src/main/java/Common/Other/SimpleTest.java
+++ examples-java/src/main/java/Common/Other/SimpleTest.java
@@ -8,28 +8,36 @@ import java.util.Scanner;
  */
 public class SimpleTest {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int length = input.nextInt();
-        String s = input.next();
-        int count = 0;
+        String a = "abc";
 
-        if (isUpperCase(s.charAt(0))) {
-            count++;
-        }
-
-        boolean flag = false;
-        for (int i = 1, j = i - 1; i < length; i++, j++) {
-
-            if (!isUpperCase(s.charAt(i)) && isUpperCase(s.charAt(j)) && !flag) {
-                count++;
-            }
-
-            if (isUpperCase(s.charAt(i)) && !isUpperCase(s.charAt(j)) && !flag) {
-                count++;
-            }
-        }
-
-        System.out.println(count + length);
+        String d = a.intern();
+        String b = new String("abc");
+        String c = "abc";
+        System.out.println(a == b);
+        System.out.println(a == c);
+        System.out.println(a == d);
+//        Scanner input = new Scanner(System.in);
+//        int length = input.nextInt();
+//        String s = input.next();
+//        int count = 0;
+//
+//        if (isUpperCase(s.charAt(0))) {
+//            count++;
+//        }
+//
+//        boolean flag = false;
+//        for (int i = 1, j = i - 1; i < length; i++, j++) {
+//
+//            if (!isUpperCase(s.charAt(i)) && isUpperCase(s.charAt(j)) && !flag) {
+//                count++;
+//            }
+//
+//            if (isUpperCase(s.charAt(i)) && !isUpperCase(s.charAt(j)) && !flag) {
+//                count++;
+//            }
+//        }
+//
+//        System.out.println(count + length);
     }
 
     public static boolean isUpperCase(char c) {
