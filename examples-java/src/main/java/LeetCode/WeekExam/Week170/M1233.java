486f12741c3b1c7c87166c201242f895694b4cfe
lucian <504866487@qq.com>
2020-01-05 10:56:03 +0800

    update leetcode week exam 1233

--- examples-java/src/main/java/LeetCode/WeekExam/Week170/M1233.java
+++ examples-java/src/main/java/LeetCode/WeekExam/Week170/M1233.java
@@ -7,4 +7,16 @@ public class M1233 {
     public static void main(String[] args) {
 
     }
+    
+    public int[] xorQueries(int[] arr, int[][] queries) {
+        int[] res = new int[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int m = queries[i][0];
+            int n = queries[i][1];
+            for (int j = m; j <= n; j++) {
+                res[i] ^= arr[j];
+            }
+        }
+        return res;
+    }
 }
