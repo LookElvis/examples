8983998bf65906f9e1fc4831fa11814f57e38ced
lucian <504866487@qq.com>
2020-01-12 10:37:18 +0800

    update

--- examples-java/src/main/java/LeetCode/WeekExam/Week171/E1236.java
+++ examples-java/src/main/java/LeetCode/WeekExam/Week171/E1236.java
@@ -7,4 +7,23 @@ public class E1236 {
     public static void main(String[] args) {
 
     }
+
+    public int[] getNoZeroIntegers(int n) {
+        for (int i = 1; i <= n / 2; i++) {
+            if (!hasZero(i) && !hasZero(n - i)) {
+                return new int[] {i, n - i};
+            }
+        }
+        return new int[] {};
+    }
+
+    public boolean hasZero(int n) {
+        while (n != 0) {
+            if (n % 10 == 0) {
+                return true;
+            }
+            n /= 10;
+        }
+        return false;
+    }
 }
