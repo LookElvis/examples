327535c57d20b830d97a67f781088078e7b9688e
lucian <504866487@qq.com>
2020-01-14 17:11:01 +0800

    update n1

--- examples-java/src/main/java/LeetCode/NewCode/N1.java
+++ examples-java/src/main/java/LeetCode/NewCode/N1.java
@@ -18,6 +18,8 @@ public class N1 {
         if (left * right > 0) {
             return Math.min(left, right) + 1;
         } else {
+            // 相乘小于等于0说明某个孩子为null，该节点不是叶子节点
+            // 所以需要返回非null孩子的深度
             return Math.max(left, right) + 1;
         }
     }
