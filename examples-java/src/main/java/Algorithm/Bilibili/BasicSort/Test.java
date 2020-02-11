ae9714e5f1f3050a26f7e4e7e10a3add7618a275
lucian <504866487@qq.com>
2020-02-11 12:37:11 +0800

    sort practice

--- examples-java/src/main/java/Algorithm/Bilibili/BasicSort/Test.java
+++ examples-java/src/main/java/Algorithm/Bilibili/BasicSort/Test.java
@@ -1,44 +1,49 @@
 package Algorithm.Bilibili.BasicSort;
 
+import java.util.Random;
+
 /**
  * Created by Elvis on 2020/2/8.
  */
 public class Test {
     public static void main(String[] args) {
-    	int[] arr = new int[]{1, 3, 7, 4, 8, 2};
-		mergeSort(arr, 0, arr.length - 1);
+    	int[] arr = new int[]{1, 3, 7, 2, 4, 8, 2, 2};
+		quickSort(arr, 0, arr.length - 1);
     	for (int i = 0; i < arr.length; i++) {
     		System.out.print(arr[i] + " ");
     	}
     }
 
-    public static void mergeSort(int[] arr, int l, int r) {
-    	if (l == r) {
-    		return;
+    public static void quickSort(int[] arr, int l, int r) {
+    	if (l < r) {
+    		int[] partition = partition(arr, l, r);
+    		quickSort(arr, l, partition[0]);
+    		quickSort(arr, partition[1], r);
     	}
-
-    	int mid = l + ((r - l) >> 1);
-    	mergeSort(arr, l, mid);
-    	mergeSort(arr, mid + 1, r);
-    	merge(arr, l, mid + 1, r);
     }
 
-    public static void merge(int[] arr, int left, int mid, int right) {
-    	int[] helper = new int[right - left + 1];
-    	int i = left;
-    	int j = mid;
-    	int index = 0;
-    	while (i < mid && j < right) {
-    		helper[index++] = arr[i] < arr[j] ? arr[i++] : arr[j++];
+    public static int[] partition(int[] arr, int l, int r) {
+    	int pivlot = arr[l + (int) Math.random() * (r - l + 1)];
+    	int p0 = l - 1;
+    	int p2 = r + 1;
+    	while (l < p2) {
+    		if (arr[l] < pivlot) {
+    			swap(arr, ++p0, l++);
+    		} else if (arr[l] > pivlot){
+    			swap(arr, --p2, l);
+    		} else {
+    			l++;
+    		}
     	}
-    	while (i < mid) {
-    		helper[index++] = arr[i++];
+    	return new int[] {p0, p2};
+    }
+
+    public static void swap (int[] arr, int i, int j) {
+    	if (i == j) {
+    		return;
     	}
-    	while (j < right) {
-    		helper[index++] = arr[j++];
-    	}
-    	for (int t = 0; t < helper.length; t++) {
-    		arr[left + t] = helper[t];
-    	}
+    	arr[i] ^= arr[j];
+    	arr[j] ^= arr[i];
+    	arr[i] ^= arr[j];
     }
 }
