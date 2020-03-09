3f3dc8516a1e3cf038207844615265e72b9ac4d6
lucian <504866487@qq.com>
2020-03-09 17:55:05 +0800

    update readWriteLock

---
+++ examples-java/src/main/java/Common/ThreadTest/ReadWriteLock.java
@@ -0,0 +1,37 @@
+package Common.ThreadTest;
+
+/**
+ * Created by Elvis on 2020/3/9.
+ */
+public class ReadWriteLock {
+    private int readLock = 0;
+    private int writeLock = 0;
+
+    public synchronized void lockRead() throws Exception{
+        while (writeLock > 0) {
+            wait();
+        }
+        readLock++;
+    }
+
+    public synchronized void unlockRead() {
+        readLock--;
+        notifyAll();
+    }
+
+    public synchronized void lockWrite() throws Exception{
+        while (writeLock > 0) {
+            wait();
+        }
+        writeLock++;
+        notifyAll();
+        while (readLock > 0) {
+            wait();
+        }
+    }
+
+    public synchronized void unlockWrite() {
+        writeLock--;
+        notifyAll();
+    }
+}
