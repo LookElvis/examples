985f91aa4648dfc691d36367676f391046a26ecc
lucian <504866487@qq.com>
2020-03-10 20:36:26 +0800

    update printOrder

---
+++ examples-java/src/main/java/Common/ThreadTest/threadPrint.java
@@ -0,0 +1,87 @@
+package Common.ThreadTest;
+
+/**
+ * Created by Elvis on 2020/3/9.
+ */
+public class threadPrint {
+    private int orderNum = 3;
+    public static int num = 1;
+    public static void main(String[] args) {
+        threadPrint orderThread = new threadPrint();
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    orderThread.printA();
+                }
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    orderThread.printB();
+                }
+            }
+        });
+        Thread t3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    orderThread.printC();
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+
+    public synchronized void printA(){
+        while (orderNum != 3) {
+            try {
+                wait();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        orderNum = 2;
+        if (num <= 100) {
+            System.out.println("This is A " + num);
+        }
+        num++;
+        notifyAll();
+    }
+
+    public synchronized void printB() {
+        while (orderNum != 2) {
+            try {
+                wait();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        orderNum = 1;
+        if (num <= 100) {
+            System.out.println("This is B " + num);
+        }
+        num++;
+        notifyAll();
+    }
+
+    public synchronized void printC() {
+        while (orderNum != 1) {
+            try {
+                wait();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        orderNum = 3;
+        if (num <= 100) {
+            System.out.println("This is C " + num);
+        }
+        num++;
+        notifyAll();
+    }
+}
