fe55f4d6a94a6bf49bbf9ee20254134a5a429f2a
lucian <504866487@qq.com>
2020-03-20 19:50:26 +0800

    update utils

--- examples-java/src/main/java/PublicClass/Utils.java
+++ examples-java/src/main/java/PublicClass/Utils.java
@@ -8,11 +8,22 @@ import java.util.Queue;
  * Created by liuxiang on 2018/10/11.
  */
 public class Utils {
+    // 求1-N的逆元,避免另一种方式超时
+    public static long[] reverseArray(int N, int MOD) {
+//        int N = 200000 + 5;
+//        int MOD = (int) 1e9 + 7;
+        long inv[] = new long[N];
+        inv[1] = 1;
+        for (int i = 2; i < N; i++) {
+            inv[i] = (MOD - MOD / i) * 1L * inv[MOD % i] % MOD;
+        }
+        return inv;
+    }
+
     // 某个分数的逆元 = 分子 * (分母 % p的逆元) = 分子 * Fermat(分母, p)
     public static long Fermat(long a, long p){//费马求a关于b的逆元
         return pow_mod(a, p - 2, p);
     }
-
     public static long pow_mod(long a, long b, long p){//a的b次方求余p
         long ret = 1;
         while(b != 0){
